68b5df622e253d209693bdc61649ca4ed88a7570
HieuNT2224 <133959877+hieunthe163894@users.noreply.github.com>
2024-10-09 20:47:33 +0700

    Update authentication.js

--- Server/controller/authentication.js
+++ Server/controller/authentication.js
@@ -98,8 +98,6 @@ const verifyUser = async (req, res) => {
 const login = async (req, res) => {
   try {
     const role = req.body.role;
-    console.log(req.body);
-    
     const existingAccount = await AccountRepository.findAccountByEmail(
       req.body.email
     );
